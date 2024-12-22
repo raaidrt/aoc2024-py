1fd07d21cf4b9767cc3227b275e9c7ecd011d47a
Raaid Tanveer <raaidraiyantanveer@gmail.com>
2024-12-21 22:29:53 -0800

    day 21 part A

--- day21.py
+++ day21.py
@@ -1,7 +1,5 @@
 import sys 
 
-from collections import defaultdict
-
 keypad = [ "789" ,
                       "456" ,
                       "123 ",
@@ -16,118 +14,94 @@ def parse():
         codes.append(line.strip())
     return codes
 
-def coords(grid: list[str]) -> defaultdict[str, tuple[int, int] | None]:
-    result : defaultdict[str, tuple[int, int] | None]= defaultdict(lambda: None)
-    for i, row in enumerate(grid):
-        for j, c in enumerate(row):
-            result[c] = i, j
-    return result
+from copy import deepcopy as dc
 
-def directions(pad: list[str], code: str): 
-    cmap = coords(pad)
-    curr = "A"
+class Lattice:
+    def __init__(self, grids: list[list[str]]): 
+        self.maps: list[dict[str, tuple[int, int]]] = []
+        self.grids: list[list[str]] = dc(grids)
+        # print(self.grids)
+        for g in grids:
+            d : dict[str, tuple[int, int]] = { }
+            for i, row in enumerate(g):
+                for j, c in enumerate(row): 
+                    d[c] = i, j
+            self.maps.append(d)
 
-    seq: list[str] = []
-    for c in code:
-        res = cmap[c]
-        if res is None: 
-            raise Exception("Missing map")
-        i, j = res
-        res = cmap[curr]
-        if res is None: 
-            raise Exception("Missing map")
-        ci, cj = res
-        hori = ">"
-        if j < cj:
-            hori = "<"
-        vert = "v"
-        if i < ci:
-            vert = "^"
-        
-        s = ""
-        v_seq = vert * abs(i - ci)
-        h_seq = hori * abs(j - cj)
+    def transition(self, curr: str) -> set[str]:
+        dirs = { "<": (0, -1), ">": (0, 1), "^": (-1, 0), "v": (1, 0) }
+        result : set[str] = set()
+        for move in ["<", ">", "^", "v", "A"]:
+            curr = move + curr[1:]
+            if move in dirs:
+                di, dj = dirs[move]
+                i, j = self.maps[1][curr[1]]
+                if 0 <= i + di < len(self.grids[1]) and 0 <= j + dj < len(self.grids[1][0]):
+                    result.add(move + self.grids[1][i + di][j + dj] + curr[2:])
+            else:
+                assert(move == "A")
 
-        if len(pad) == 4:
-            match (hori, vert):
-                case ("<", "^"): 
-                    s = v_seq + h_seq
-                case (">", "v"): 
-                    s = h_seq + v_seq
-                case _:
-                    s = h_seq + v_seq
-        elif len(pad) == 2:
-            match (hori, vert):
-                case (">", "^"): 
-                    s = h_seq + v_seq
-                case ("<", "v"): 
-                    s = v_seq + h_seq
-                case _:
-                    s = h_seq + v_seq
+                first_idx_non_a = len(curr)
+                for i, c in enumerate(curr):
+                    if c == "A": 
+                        continue
+                    first_idx_non_a = i
+                    break
 
-        seq.append(s)
-        seq.append("A")
-        curr = c
+                if first_idx_non_a >= len(curr) - 1:
+                    result.add(curr)
+                
+                if first_idx_non_a < len(curr) - 1:
+                    move = curr[first_idx_non_a]
+                    di, dj = dirs[move]
+                    i, j = self.maps[first_idx_non_a + 1][curr[first_idx_non_a + 1]]
+                    if 0 <= i + di < len(self.grids[first_idx_non_a + 1]) and 0 <= j + dj < len(self.grids[first_idx_non_a + 1][0]):
+                        result.add(curr[:first_idx_non_a + 1] + self.grids[first_idx_non_a + 1][i + di][j + dj] + curr[first_idx_non_a + 2:])
 
-    return ''.join(seq)
+        result = { s for s in result if "." not in s }
+        return result
+    
+    def bfs(self, frontier: set[str], dest: str) -> int | None:
+        visited : set[str] = set()
+        counter = 0
+        while len(frontier) > 0 and dest not in frontier:
+            # print(frontier)
+            visited = visited.union(frontier)
+            new_frontier : set[str] = set()
+            for node in frontier:
+                new_frontier = new_frontier.union(nbor for nbor in self.transition(node) if nbor not in visited)
+            counter += 1
+            frontier = new_frontier
+        if dest in frontier: 
+            return counter  
+        else: return None
 
-def verify(grid: list[str], moves: str) -> str:
-    result : list[str] = []
-    cmap: dict[str, tuple[int, int]] = { }
-    for i, row in enumerate(grid):
-        for j, c in enumerate(row):
-            cmap[c] = i, j
-    curr = "A"
-    dirs = { ">": (0, 1), "<": (0, -1), "^": (-1, 0), "v": (1, 0) }
-    for move in moves:
-        if move == "A": 
-            result.append(curr)
-        else:
-            di, dj = dirs[move]
-            i, j = cmap[curr]
-            curr = grid[i + di][j + dj]
-    return ''.join(result)
+for l in range(2, 10):
+    graph = Lattice([direction] * (l - 1) + [direction])
+    start = "A" * (l - 1) + ">"
+    end = "A" * (l - 1) + "^"
 
-def check(levels: int, moves: str) -> str:
-    for level in range(levels):
-        grid = direction
-        if level == levels - 1:
-            grid = keypad
-        
-        moves = verify(grid, moves)
-        print(f"\t level = {level} verification = {moves}")
-    return moves
+    print(f"{start} -> {end}, bfs = {graph.bfs({ start }, end)}")
 
-DEBUG: bool = True
-def dp(x):
-    if DEBUG:
-        print(x)
-
-print(f"verification for 379A = {check(3, '<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A')}")
+LEVELS: int = 4
 
 def main():
     codes = parse()
-    
+    graph = Lattice([direction] * (LEVELS - 1) + [keypad])
     total = 0
     for code in codes:
         num = int(code[:-1])
+        init = "A" * LEVELS
+        count = 0
+        for c in code:
+            dest = "A" * (LEVELS - 1) + c
+            res = graph.bfs({ init }, dest)
+            if res == None: 
+                raise Exception(f"Failed for init = {init} dest = {dest} ")
+            count += res            
+            init = dest
         
-        dp(f"processing code {code}")
-        dirs = directions(keypad, code)
-        dp(dirs)
-        print(f"verification {check(1, dirs)}")
-        dirs = directions(direction, dirs)
-        dp(dirs)
-        print(f"verification {check(2, dirs)}")
-        dirs = directions(direction, dirs)
-        dp(dirs)
-
-        print(f"verification {check(3, dirs)}")
-        
-        dp("")
-
-        total += num * len(dirs)
-
+        total += num * count
     print(total)
 
 if __name__ == "__main__": 
